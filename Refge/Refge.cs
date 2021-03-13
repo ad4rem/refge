a7f3d6fe577be1ebec674c9a14bceb4481c4f07d
ad4rem <61497134+ad4rem@users.noreply.github.com>
2021-03-13 12:40:33 +0200

    Start version

---
+++ Refge/Refge.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Refge
+{
+    public static class Refge
+    {
+        public static string GetVersion(string version)
+        {
+            return "v" + version;
+        }
+    }
+}
